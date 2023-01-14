c5bee9a930c4752548fa357694735eff1f5d9adb
wallas5h@gmail.com <wallas5h@gmail.com>
2023-01-14 13:45:27 +0100

    add endpoits for adding user to dislike list, disActiveRoom, addUserToRoomMembers, updateRoomData

--- routes/roomsRouter.ts
+++ routes/roomsRouter.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { createRoom, getRooms } from "../controllers/roomsController";
+import {
+  addUserToDislikeList,
+  addUserToRoomMembers,
+  createRoom,
+  disActiveRoom,
+  getRooms,
+  updateRoomData,
+} from "../controllers/roomsController";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 export const roomsRouter = Router();
 
 roomsRouter
   .get("/", authMiddleware, getRooms)
-  .post("/", authMiddleware, createRoom);
+  .post("/", authMiddleware, createRoom)
+  .patch("/", authMiddleware, addUserToDislikeList)
+  .delete("/:id", authMiddleware, disActiveRoom)
+  .patch("/:id", authMiddleware, addUserToRoomMembers)
+  .put("/:id", authMiddleware, updateRoomData);
