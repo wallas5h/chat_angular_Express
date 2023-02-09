52a01322b29bfd5146af6d7054320102b0fe7e3f
wallas5h@gmail.com <wallas5h@gmail.com>
2023-02-09 22:52:17 +0100

    add endpoints for get and update messages

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {
   findByName,
+  getNewMessages,
+  getOneUser,
   getUsers,
   loginUser,
   logoutUser,
   signupUser,
+  updateNewMessages,
 } from "../controllers/userController";
 import { authMiddleware } from "../middleware/authMiddleware";
 
@@ -12,7 +15,10 @@ export const userRouter = Router();
 
 userRouter
   .get("/", authMiddleware, getUsers)
+  .get("/messages", authMiddleware, getNewMessages)
+  .get("/:id", authMiddleware, getOneUser)
   .post("/signup", signupUser)
   .post("/login", loginUser)
+  .post("/messages", authMiddleware, updateNewMessages)
   .delete("/logout", authMiddleware, logoutUser)
   .post("/find", authMiddleware, findByName);
