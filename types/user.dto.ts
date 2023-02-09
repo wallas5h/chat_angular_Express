271f6052d5629d1481f1d4e0806e6deae02b51fe
wallas5h@gmail.com <wallas5h@gmail.com>
2023-02-09 22:47:39 +0100

    update user types

--- types/user.dto.ts
+++ types/user.dto.ts
@@ -14,3 +14,24 @@ export interface UserEntity {
   token?: string | undefined;
   refreshToken?: string | undefined;
 }
+
+export interface UserEntitySocket {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  newMessages: any;
+  status: string;
+  image?: string | undefined;
+  token?: string | undefined;
+  refreshToken?: string | undefined;
+}
+
+export interface UserResponseDto {
+  id: string;
+  name: string;
+  email?: string;
+  image: string;
+  status: string;
+  newMessages?: string;
+}
